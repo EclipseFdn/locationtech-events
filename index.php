2b34dfb58a5a7aee2936d73d29e584a0a047fc79
Andrew Ross <fosslc@gmail.com>
2013-09-09 16:15:58 -0400

    set maxZoom to 8 temporarily

--- index.php
+++ index.php
@@ -208,7 +208,7 @@
    	 	map.doubleClickZoom.disable();
     	L.tileLayer('http://{s}.tile.cloudmade.com/2657344f891e48bcb4defa3bd7c32f77/107312/256/{z}/{x}/{y}.png', {
     	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-        maxZoom: 18,
+        maxZoom: 8,
         minZoom: 6
     	}).addTo(map);
     	<?php print $event->getJavascript();?>
