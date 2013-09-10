022a4db679b43f45b44045ece9884ebdf822e55c
Christopher Guindon <chris.guindon@eclipse.org>
2013-09-09 21:34:06 -0400

    fix "next" event header on mobile.

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
         <div class="row">
           <div class="col-md-6">
             <div id="next-event" class="col-md-12">
-							<div id="event-header">
+							<div id="event-header" class="clearfix">
 								<h2>Next Event<img src="assets/images/location-sm-icon16x16.gif" alt="locationtech logo"/>
 								  <span id="event-location"><?php print $next['city']?></span>
 								</h2>
@@ -211,7 +211,7 @@
     	   minZoom: 1,
     	   scrollWheelZoom: false,
          zoomControl: false
-        	}).setView([43.358431, -71.059773], 6);
+      }).setView([43.358431, -71.059773], 6);
    	 	map.doubleClickZoom.disable();
     	L.tileLayer('http://{s}.tile.cloudmade.com/2657344f891e48bcb4defa3bd7c32f77/107312/256/{z}/{x}/{y}.png', {
     	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
