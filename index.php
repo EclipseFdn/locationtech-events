086aaeaf3a72f6a638668cbe7456f03321c37aa9
Christopher Guindon <chris.guindon@eclipse.org>
2013-10-04 14:44:15 -0400

    Andrew requested a page with only the sponsors. He plan on using this in an iFrame on eventbrite.

--- index.php
+++ index.php
@@ -128,17 +128,7 @@
       </div>
     </div>
 
-		<div id="sponsors">
-			<div class="container clearfix">
-				<ul class="list-inline">
-					<li><a href="http://azavea.com/" title="Azavea" target="_blank"><img src="assets/images/sponsors/Azavea.png" height="39" width="160" alt="Azavea"/></a></li>
-					<li><a href="http://boundlessgeo.com/" title="Boundlessgeo" target="_blank"><img src="assets/images/sponsors/Boundlessgeo.png" alt="Boundlessgeo"/></a></li>
-					<li><a href="http://google.com/" title="Google" target="_blank"><img src="assets/images/sponsors/Google.png" alt="Google"/></a></li>
-					<li><a href="http://departments.columbian.gwu.edu/geography/" title="George Washington University" target="_blank"><img src="assets/images/sponsors/GWU.png" height="84" width="160" alt="George Washington University"/></a></li>
-					<li><a href="http://elliott.gwu.edu/" title="Elliott School of International Affairs" target="_blank"><img src="assets/images/sponsors/ESIA.jpg" height="52" width="160" alt="Elliott School of International Affairs"/></a></li>
-				</ul>
-			</div>
-		</div>
+		<?php include_once('assets/tpl/sponsors.tpl.php');?>
 
     <footer>
       <div class="container">
