6a3f7bfab2d40a85aa8e358e60e005d30f5ec643
Andrew Ross <fosslc@gmail.com>
2013-10-08 20:37:36 -0400

    Add appgeo as Gold sponsor

--- assets/tpl/sponsors.tpl.php
+++ assets/tpl/sponsors.tpl.php
@@ -4,6 +4,8 @@
 		<ul class="list-inline">
 		  <li><a href="http://azavea.com/" title="Azavea" target="_blank"><img src="assets/images/sponsors/Azavea.png" height="39" width="160" alt="Azavea"/></a></li>
 			<li><a href="http://boundlessgeo.com/" title="Boundlessgeo" target="_blank"><img src="assets/images/sponsors/Boundlessgeo.png" alt="Boundlessgeo"/></a></li>
+			<li><a href="http://www.appgeo.com/" title="Applied Geographics" target="_blank"><img src="assets/images/sponsors/appgeo.jpg" height="63" width="160" alt="Applied Geographics"/></a></li>
+
 			<li><a href="http://google.com/" title="Google" target="_blank"><img src="assets/images/sponsors/Google.png" alt="Google"/></a></li>
 			<li><a href="http://departments.columbian.gwu.edu/geography/" title="George Washington University" target="_blank"><img src="assets/images/sponsors/GWU.png" height="84" width="160" alt="George Washington University"/></a></li>
 			<li><a href="http://elliott.gwu.edu/" title="Elliott School of International Affairs" target="_blank"><img src="assets/images/sponsors/ESIA.jpg" height="52" width="160" alt="Elliott School of International Affairs"/></a></li>
@@ -14,6 +16,7 @@
 			<li><a href="http://www.hackreduce.org" title="hack/reduce" target="_blank"><img src="assets/images/sponsors/hr-header.jpg" height="80" width="160" alt="hack/reduce"/></a></li>
 
 			<li><a href="http://www.gis.harvard.edu/" title="Center For Geographic Analysis" target="_blank"><img src="assets/images/sponsors/cga.jpg" height="80" width="160" alt="Center For Geographic Analysis"/></a></li>
+
 		</ul>
   </div>
 </div>
