792793f20d8de6f03522fcfd73d89164d64442b9
Andrew Ross <andrew.ross@eclipse.org>
2014-02-19 10:58:29 -0500

    Reset events and sponsors for start of 2014 tour, work in progress

--- assets/tpl/sponsors.tpl.php
+++ assets/tpl/sponsors.tpl.php
@@ -2,28 +2,15 @@
 	<div class="container clearfix">
 	  <h2>Our Sponsors</h2>
 		<ul class="list-inline">
-		  <li><a href="http://azavea.com/" title="Azavea" target="_blank"><img src="assets/images/sponsors/Azavea.png" height="39" width="160" alt="Azavea"/></a></li>
+ 		        <li><a href="http://azavea.com/" title="Azavea" target="_blank"><img src="assets/images/sponsors/Azavea.png" height="39" width="160" alt="Azavea"/></a></li>
 			<li><a href="http://boundlessgeo.com/" title="Boundlessgeo" target="_blank"><img src="assets/images/sponsors/Boundlessgeo.png" alt="Boundlessgeo"/></a></li>
-			<li><a href="http://www.appgeo.com/" title="Applied Geographics" target="_blank"><img src="assets/images/sponsors/appgeo.jpg" height="63" width="160" alt="Applied Geographics"/></a></li>
+			<!--<li><a href="http://www.appgeo.com/" title="Applied Geographics" target="_blank"><img src="assets/images/sponsors/appgeo.jpg" height="63" width="160" alt="Applied Geographics"/></a></li>-->
 
-			<li><a href="http://www.mapgears.com/" title="MapGears" target="_blank"><img src="assets/images/sponsors/MapGears.png" height="87" width="160" alt="MapGears"/></a></li>
+			<!--<li><a href="http://www.mapgears.com/" title="MapGears" target="_blank"><img src="assets/images/sponsors/MapGears.png" height="87" width="160" alt="MapGears"/></a></li>-->
 
-			<li><a href="http://www.twitter.com/" title="Twitter" target="_blank"><img src="assets/images/sponsors/twitter.png" height="109" width="128" alt="Twitter"/></a></li>
+			<!--<li><a href="http://www.twitter.com/" title="Twitter" target="_blank"><img src="assets/images/sponsors/twitter.png" height="109" width="128" alt="Twitter"/></a></li>-->
 
-<!--
-			<li><a href="http://google.com/" title="Google" target="_blank"><img src="assets/images/sponsors/Google.png" alt="Google"/></a></li>
--->
-
-			<li><a href="http://departments.columbian.gwu.edu/geography/" title="George Washington University" target="_blank"><img src="assets/images/sponsors/GWU.png" height="84" width="160" alt="George Washington University"/></a></li>
-
-			<li><a href="http://www.gis.harvard.edu/" title="Center For Geographic Analysis" target="_blank"><img src="assets/images/sponsors/cga.jpg" height="80" width="160" alt="Center For Geographic Analysis"/></a></li>
-
-			<li><a href="http://www.hackreduce.org" title="hack/reduce" target="_blank"><img src="assets/images/sponsors/hr-header.jpg" height="80" width="160" alt="hack/reduce"/></a></li>
-			<li><a href="http://departments.columbian.gwu.edu/geography/" title="George Washington University Geography and Environmental Studies" target="_blank"><img src="assets/images/sponsors/GWU_geo.jpg" height="81" width="160" alt="George Washington University Geography and Environmental Studies"/></a></li>
-
-			<li><a href="http://arboretum.harvard.edu/" title="The Arnold Arboretum of HARVARD UNIVERISTY" target="_blank"><img src="assets/images/sponsors/aa-logotype.png" height="49" width="160" alt="The Arnold Arboretum of HARVARD UNIVERISTY"/></a></li>
-
-			<li><a href="http://elliott.gwu.edu/" title="Elliott School of International Affairs" target="_blank"><img src="assets/images/sponsors/ESIA.jpg" height="52" width="160" alt="Elliott School of International Affairs"/></a></li>
+			<li><a href="http://oracle.com/" title="Oracle" target="_blank"><img src="assets/images/sponsors/oracle.png" alt="Oracle"/></a></li>
 
 
 		</ul>
