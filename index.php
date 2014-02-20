5edd975d164b65346227d4424a6b8a714eec330e
Andrew Ross <andrew.ross@eclipse.org>
2014-02-20 16:23:00 -0500

    Shift default position a bit more north

--- index.php
+++ index.php
@@ -206,7 +206,7 @@
     		 maxZoom: 7,
     	   minZoom: 1,
     	   scrollWheelZoom: false
-      }).setView([29.310000, -55.310000], 3);
+      }).setView([40.000000, -55.310000], 3);
    	 	map.doubleClickZoom.disable();
     	L.tileLayer('http://{s}.tile.cloudmade.com/2657344f891e48bcb4defa3bd7c32f77/107312/256/{z}/{x}/{y}.png', {
     	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
