81580568f199023f98864d02cc71bf107a3622b2
Andrew Ross <andrew.ross@eclipse.org>
2014-02-21 09:12:34 -0500

    used another example to figure out the right js library for leaflet.label

--- class.events.php
+++ class.events.php
@@ -123,8 +123,7 @@ class Events {
       $coordinate = $c['latitude'] . ', ' . $c['longitude'];
 
       // Create a dynamic label for each city
-      //$this->javascript .= "var " . $id  . " = L.marker([" . $coordinate . "]).bindLabel('$id', { noHide: true }).addTo(map);" . PHP_EOL;
-      $this->javascript .= "var " . $id  . " = L.marker([" . $coordinate . "]).addTo(map);" . PHP_EOL;
+      $this->javascript .= "var " . $id  . " = L.marker([" . $coordinate . "]).bindLabel('$id', { noHide: true }).addTo(map);" . PHP_EOL;
       $this->javascript .=  $id  . '.bindPopup(\''. $body .'\');' . PHP_EOL;
     }
   }
