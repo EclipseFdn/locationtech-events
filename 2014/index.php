f87638af7d8f5e06be12c407bcf86d2892e03049
Andrew Ross <andrew.ross@eclipse.org>
2014-07-02 13:46:53 -0400

    Revert "focus on upcoming for now"
    This reverts commit 793ece7cc1ce798d97c7a366e8122ff929f94814.

--- 2014/index.php
+++ 2014/index.php
@@ -111,6 +111,8 @@
 						<div class="row" id="other-dates-container">
 						  <div class="col-xs-12">
         				<?php print $event->upcomingEvents();?>
+        				<h2>Past Events</h2>
+        				<?php print $event->pastEvents();?>
         				<div class="clearfix"></div>
         				<div class="container">
 						  		<span class="st_sharethis_large" displayText="ShareThis"></span>
